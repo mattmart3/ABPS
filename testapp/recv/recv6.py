6586ca186361598db27d7b7ca2ca8749d28e9d59
matteomartelli <matteomartelli3@gmail.com>
2015-08-29 22:32:59 +0200

    removed json from recv too. removed data analysis scripts

--- testapp/recv/recv6.py
+++ testapp/recv/recv6.py
@@ -1,5 +1,6 @@
-#Recv ipv4 version.
-#TODO: merge with recv6 (ipv6 version)
+#Recv ipv6 version.
+#TODO: merge with recv4 (ipv4 version)
+
 import sys, os
 
 import socket
@@ -10,8 +11,9 @@ import json
 
 import time
 
+#example: python recv6.py fe80::226:b0ff:fed7:7dfa%eth0 50000
 
-parser = argparse.ArgumentParser(description="IPv4 address and port")
+parser = argparse.ArgumentParser(description="IPv6 address and port")
 parser.add_argument('address')
 parser.add_argument('port')
 args = parser.parse_args()
@@ -20,10 +22,10 @@ address = args.address
 
 port = int(args.port)
 
-receivingSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+receivingSocket = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
 print "Socket primed."
 
-result = socket.getaddrinfo(address, port, socket.AF_INET, socket.SOCK_DGRAM, 0, 0)
+result = socket.getaddrinfo(address, port, socket.AF_INET6, socket.SOCK_DGRAM, 0, 0)
 
 family, socketType, proto, canonName, socketAddress = result[0]
 receivingSocket.bind(socketAddress)
