813c1c33b21c60b6162a7c59ce5b8f1d274d0534
matteomartelli <matteomartelli3@gmail.com>
2015-08-31 18:27:50 +0200

    license added

--- linux/ABPS_mac80211.c
+++ linux/ABPS_mac80211.c
@@ -1,3 +1,19 @@
+/*
+ *	Trasmission Error Detector core functions.
+ *
+ *	Authors: 
+ *	TODO add authors
+ *
+ *
+ *	Fixes:
+ *	Matteo Martelli:  Fixed the ipv6 fragment information retrieval 
+ *
+ *	This program is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License
+ *	as published by the Free Software Foundation; either version
+ *	2 of the License, or (at your option) any later version.
+ */
+
 /* ABPS_ieee80211.c */
 
 #include <net/mac80211.h>
